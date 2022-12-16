0ffac30d37ec128f437a7bf697cdaabaf9f4aba7
JuliaKozachuk <uliakozacuk649@gmail.com>
2022-12-16 22:32:08 +0300

    feet: added log

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"net/http"
 
+	"github.com/JuliaKozachuk/BackChat/migrations"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	route := gin.Default()
 
+	migrations.ConnectDB() // new
+
 	route.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
+		context.JSON(http.StatusOK, gin.H{"message": "Успешное соединение"})
 	})
 
 	route.Run()
-
 }
