c8966ac27ceb809737e9bb2dda83b50afb896d8e
JuliaKozachuk <uliakozacuk649@gmail.com>
2022-12-20 17:09:35 +0300

    feet:added log

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	"log"
 	"os"
 
-	"github.com/JuliaKozachuk/BackChat/connectredis"
-	"github.com/joho/godotenv"
-
 	"github.com/JuliaKozachuk/BackChat/controllers"
-
 	"github.com/JuliaKozachuk/BackChat/migrations"
+	"github.com/JuliaKozachuk/BackChat/redisconnect"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+//var ctx = context.Background()
+
 func main() {
-	connectredis.ExampleClient()
+
+	redisconnect.ExampleClient()
 
 	err := godotenv.Load()
 	if err != nil {
@@ -33,11 +35,27 @@ func main() {
 
 	route := gin.Default()
 
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379",
+	// 	Password: "", // no password set
+	// 	DB:       0,  // use default DB
+	// })
+
+	// err := rdb.Set(ctx, "key", "value", 0).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// val, err := rdb.Get(ctx, "key").Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("key", val)
+
 	migrations.ConnectDB(postgres_data)
 
 	route.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "Успешное соединение"})
-
 	})
 	route.GET("/userID", controllers.GetAllUsers)
 	route.POST("/user", controllers.CreateUser)
