381008400c07623a18d35871a7d442872e5cfa74
JuliaKozachuk <uliakozacuk649@gmail.com>
2022-12-20 18:44:47 +0300

    rollback to previous settings

--- main.go
+++ main.go
@@ -1,34 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/JuliaKozachuk/BackChat/controllers"
 	"github.com/JuliaKozachuk/BackChat/migrations"
 	"github.com/JuliaKozachuk/BackChat/redisconnect"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	redisconnect.ExampleClient()
 
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-	// host := os.Getenv("DB_HOST")
-	// port := os.Getenv("DB_PORT")
-	// user := os.Getenv("DB_USER")
-	// dbname := os.Getenv("DB_NAME")
-	// password := os.Getenv("DB_PASSWORD")
-	// sslmode := os.Getenv("DB_SSLMODE")
-
-	// postgres_data := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s ", host, port, user, dbname, password, sslmode)
-
 	route := gin.Default()
 
-	migrations.ConnectDB(other.postgresUrl())
+	migrations.ConnectDB(postgresUrl())
 
 	route.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "Успешное соединение"})
@@ -39,19 +30,19 @@ func main() {
 	route.Run()
 }
 
-// func postgresUrl() string {
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		log.Fatal("Error loading .env file")
-// 	}
-// 	host := os.Getenv("DB_HOST")
-// 	port := os.Getenv("DB_PORT")
-// 	user := os.Getenv("DB_USER")
-// 	dbname := os.Getenv("DB_NAME")
-// 	password := os.Getenv("DB_PASSWORD")
-// 	sslmode := os.Getenv("DB_SSLMODE")
+func postgresUrl() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+	sslmode := os.Getenv("DB_SSLMODE")
 
-// 	postgres_data := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s ", host, port, user, dbname, password, sslmode)
+	postgres_data := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s ", host, port, user, dbname, password, sslmode)
 
-// 	return postgres_data
-// }
+	return postgres_data
+}
